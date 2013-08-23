a4e547779bb288aa68349e9994e2604465f36c82
Kaustubh Duraphe <kaustubhduraphe@gmail.com>
2013-08-22 20:05:27 -0500

    Committing nalayak15

--- issue.c
+++ issue.c
@@ -2,6 +2,6 @@ if(my_name == 'kaustubh')
 	printf("hey kaustubh");
 else if(my_name == 'kastubh')
 	printf("You have wrongly typed my name");
-else
-	printf("hey there");
+else if(my_name == 'abcd')
+	printf("hey there abcd");
 
